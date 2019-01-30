2ce1428eac5a5aa9e419b5c5a2b758e6415358a9
bachdx <bachdx.hut@gmail.com>
2019-01-30 09:36:20 +0700

    test component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,19 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import Vue from 'vue/dist/vue.esm'
+
+import TurbolinksAdapter from 'vue-turbolinks'
+Vue.use(TurbolinksAdapter)
+
+import Test from './components/Test.vue'
+
+document.addEventListener('turbolinks:load', () => {
+  const vue = new Vue({
+    el: "#app",
+    components: {
+      App,
+      Test
+    }
+  });
+});
