80a55d90df3a7490b046a6dd10c132915ae72c48
bachdx <bachdx.hut@gmail.com>
2019-02-09 22:29:07 +0700

    input focus form

---
+++ app/assets/javascripts/form-helpers.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+  formInputLabelPopUp();
+}
+
+function formInputLabelPopUp() {
+  const userSelection = document.getElementsByClassName('md-input');
+  for(let i = 0; i < userSelection.length; i++) {
+    let element = userSelection[i];
+    if (element.value) {
+      element.parentElement.classList.add('md-focused');
+    } else {
+      element.parentElement.classList.remove('md-focused');
+    }
+
+    element.addEventListener('focus', function(e) {
+      e.target.parentElement.classList.add('md-focused');
+    });
+
+    element.addEventListener('focusout', function(e) {
+      if (!element.value) {
+        e.target.parentElement.classList.remove('md-focused');
+      }
+    });
+  }
+}
